babd505aeae8bd75b565143dae4221a0a99c33e7
Oleg Endo <oleg.endo@jutze.com.cn>
2015-12-25 14:12:03 +0900

    fix vec type PODness.  add conversion constructors.

--- src/utils/vec_mat.hpp
+++ src/utils/vec_mat.hpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <cmath>
 #include <algorithm>
+#include <type_traits>
 
 // --------------------------------------------------------------------------
 
@@ -21,7 +22,7 @@ template <typename T> struct vec2
     T xy[ELEMENT_COUNT];
   };
 
-  vec2 (void) { }
+  vec2 (void) = default;
 
   template <typename XY>
   vec2 (XY xy) : x (xy), y (xy) { }
@@ -29,7 +30,10 @@ template <typename T> struct vec2
   template <typename X, typename Y>
   vec2 (X xx, Y yy) : x (xx), y (yy) { }
 
-  template <typename S> operator vec2<S> () const
+  template <typename S> explicit vec2 (const vec2<S>& rhs)
+  : x (rhs.x), y (rhs.y) { }
+
+  template <typename S> explicit operator vec2<S> () const
   {
     return vec2<S> ((S)x, (S)y);
   }
@@ -105,15 +109,15 @@ template <typename T> struct vec2
 
   friend vec2 operator / (const vec2& a, const vec2& b)
   {
-    return vec4 (a.x / b.x, a.y / b.y);
+    return vec2 (a.x / b.x, a.y / b.y);
   }
   template <typename S> friend vec2 operator / (const vec2& a, S b)
   {
-    return vec4 (a.x / b, a.y / b);
+    return vec2 (a.x / b, a.y / b);
   }
   template <typename S> friend vec2 operator / (S a, const vec2& b)
   {
-    return vec3 (a / b.x, a / b.y);
+    return vec2 (a / b.x, a / b.y);
   }
   vec2& operator /= (const vec2& b)
   {
@@ -186,7 +190,7 @@ template <typename T> struct vec3
     T xyz[ELEMENT_COUNT];
   };
 
-  vec3 (void) { }
+  vec3 (void) = default;
 
   template <typename XYZ>
   vec3 (XYZ xyz) : x (xyz), y (xyz), z (xyz) { }
@@ -198,7 +202,10 @@ template <typename T> struct vec3
   template <typename XY, typename Z> vec3 (const vec2<XY>& xy, Z zz)
   : x (xy.x), y (xy.y), z (zz) { }
 
-  template <typename S> operator vec3<S> () const
+  template <typename S> explicit vec3 (const vec3<S>& rhs)
+  : x (rhs.x), y (rhs.y), z (rhs.z) { }
+
+  template <typename S> explicit operator vec3<S> () const
   {
     return vec3<S> ((S)x, (S)y, (S)z);
   }
@@ -364,7 +371,7 @@ template <typename T> struct vec4
     T xyzw[ELEMENT_COUNT];
   };
 
-  vec4 (void) { }
+  vec4 (void) = default;
 
   template <typename XYZW>
   vec4 (XYZW xyzw) : x (xyzw), y (xyzw), z (xyzw), w (xyzw) { }
@@ -379,7 +386,10 @@ template <typename T> struct vec4
   template <typename XY, typename ZW> vec4 (const vec2<XY>& xy, const vec2<ZW>& zw)
   : x (xy.x), y (xy.y), z (zw.x), w (zw.y) { }
 
-  template <typename S> operator vec4<S> () const
+  template <typename S> explicit vec4 (const vec4<S>& rhs)
+  : x (rhs.x), y (rhs.y), z (rhs.z), w (rhs.w) { }
+
+  template <typename S> explicit operator vec4<S> () const
   {
     return vec4<S> ((S)x, (S)y, (S)z, (S)w);
   }
@@ -768,5 +778,19 @@ template <typename T> struct mat4
     return m * translate (-eye);
   }
 };
+
+
+
+static_assert (std::is_trivial<vec2<float>>::value, "");
+static_assert (std::is_standard_layout<vec2<float>>::value, "");
+static_assert (std::is_pod<vec2<float>>::value, "");
+
+static_assert (std::is_trivial<vec3<int>>::value, "");
+static_assert (std::is_standard_layout<vec3<int>>::value, "");
+static_assert (std::is_pod<vec3<int>>::value, "");
+
+static_assert (std::is_trivial<vec4<int>>::value, "");
+static_assert (std::is_standard_layout<vec4<int>>::value, "");
+static_assert (std::is_pod<vec4<int>>::value, "");
 
 #endif // includeguard_vec_mat_hpp_includeguard
