7551b27d8bc5445b385150aa4b92f71b49a82db1
Oleg Endo <oleg.endo@jutze.com.cn>
2017-07-20 14:04:38 +0900

    update and fix mingw toolchains for crosscompiling

--- defaults.cmake
+++ defaults.cmake
@@ -1,8 +1,12 @@
 
-if (WIN32)
+# on linux we can't do a full static link
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  set (TARGET_FULL_STATIC OFF CACHE BOOL "")
+endif ()
 
-else ()
-  set (TARGET_FULL_STATIC OFF CACHE STRING "")
 
+# when cross compiling a DLL for windows, we have to do a full static link
+if (CMAKE_CROSSCOMPILING AND ${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  set (TARGET_FULL_STATIC ON CACHE BOOL "")
 endif ()
 
